f687234c11bd73f8c4ffe80db379d044adfca28d
ToriK17 <vfcad@protonmail.com>
2019-09-02 19:27:49 -0700

    adds more routes and formatting on time

---
+++ app/models/contact.rb
@@ -0,0 +1,11 @@
+class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}" 
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new(
+  first_name: "Bob"
+  last_name: "Bobberson"
+  email: "bb@gmail.com"
+  phone: "777-777-7777"
+  )
+contact.save
+
+contact = Contact.new(
+  first_name: "Dave"
+  last_name: "Davidson"
+  email: "dd@gmail.com"
+  phone: "777-777-7777"
+  )
+contact.save
